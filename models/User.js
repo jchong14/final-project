c8393803b3d0ba22277aba6887638f888c6f832f
Ji Chong <ji_no8@yahoo.com>
2018-10-08 20:28:30 -0700

    created user registration

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//Create Shchema
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
+
+
+//This is our user model
--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +22,7 @@ mongoose
     .connect(db)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log("error"));
-app.get('/', (req, res) => res.send("Hello"));
+app.get('/', (req, res) => res.send("Hello World"));
 
 //Use Routes
 app.use("/api/users", users);
